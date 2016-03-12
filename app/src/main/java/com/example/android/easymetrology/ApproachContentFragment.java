c252b2aa58a2c1065bb78047ba2f4394c7c5fd17
fromdenisvieira <denisvieira05@gmail.com>
2016-03-12 17:50:53 -0300

    approach result in display

--- app/src/main/java/com/example/android/easymetrology/ApproachContentFragment.java
+++ app/src/main/java/com/example/android/easymetrology/ApproachContentFragment.java
@@ -6,13 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 /**
  * Created by denisvieira on 12/03/16.
  */
-public class ApproachContentFragment extends Fragment{
+public class ApproachContentFragment extends Fragment {
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
@@ -20,6 +21,19 @@ public class ApproachContentFragment extends Fragment{
 
 //        TextView textv = (TextView) view.findViewById(R.id.textViewCaliperRule);
 //        textv.setText("Frament TESTE");
+
+        Button button = (Button) view.findViewById(R.id.calculateApproachButton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                TextView resultApproach = (TextView) getView().findViewById(R.id.resultApproach);
+                float result = calcApproach();
+
+                resultApproach.setText("APROXIMAÇÃO \n"+result+"mm");
+            }
+        });
 
         return (view);
     }
@@ -29,9 +43,8 @@ public class ApproachContentFragment extends Fragment{
 //        tv.setText("Fragmento modificado");
 //    }
 
-    public void calcApproach(){
+    public float calcApproach(){
 
-        TextView resultApproach = (TextView) getView().findViewById(R.id.resultApproach);
         EditText valorDivisoesDoNonio = (EditText) getView().findViewById(R.id.valorDivisoesDoNonio);
         EditText valorEscalaPrincipal = (EditText) getView().findViewById(R.id.valorEscalaPrincipal);
 
@@ -40,7 +53,7 @@ public class ApproachContentFragment extends Fragment{
 
         float result = valorEscala/valorDivisoes;
 
-        resultApproach.setText(String.valueOf("APROXIMAÇÃO\\n"+result+"mm"));
+        return result;
 
     }
 }
